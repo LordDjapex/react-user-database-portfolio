6448075eb8e23270d446a8f988311933f902a73b
Djapex <nikolapudja@gmail.com>
2020-07-15 17:50:56 +0200

    make it better

---
+++ backend/node.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../public'))
+}
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/user_database', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('Mongoose successfully connected')
+    }).catch(() => {
+        console.log('Mongoose connection failed')
+    });
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const contentModel = mongoose.model('users', new mongoose.Schema({
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    email: {type: String, match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, unique: true, required: true, lowercase: true}
+}))
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//functions
+
+const removeUser = ((id) => {
+    contentModel.findByIdAndDelete(id, (err, object) => {
+        if (err) {
+            console.log('Error while removing a person')
+            return {
+                err: 'error removing a person'
+            }
+        }
+    })
+})
+
+const addUser = (({name, surname, email}) => {
+    new contentModel({name, surname, email}).save().then(() =>{
+        return 'Successfully added user to the database'
+    }
+    ).catch(() => {
+        return 'Failure to add user in database'
+    });
+})
+
+let array = []
+
+function values() { 
+    db.collection('users').find(async (err, object) => {
+    if (err) {
+        console.log(err)
+        return err;
+    }
+
+    let pageNames = []
+
+    await object.forEach((doc) => {
+        const name = doc.name;
+        const surname = doc.surname
+        const email = doc.email
+        const _id = doc._id
+        pageNames.push({_id, name, surname, email})
+    })
+
+    array = pageNames
+   return pageNames
+})
+}
+
+addUser({name: 'Nemanja', surname: 'Turi', email:'exp@example.com'})
+
+//routes
+
+app.get('/', async (req, res) => {
+    await values();
+    res.json({data: array});
+})
+
+app.get('/add', (req, res) => {
+    const { name, surname, email} = req.query
+    addUser({name, surname, email})
+    res.send({data: addUser({name, surname, email})})
+})
+
+app.get('/remove', (req, res) => {
+    const {id} = req.query
+    console.log(id)
+    removeUser(id)
+    res.send({data: removeUser(id)})
+})
+
+app.listen(port, () => {
+    console.log('listening on ' + port)
+})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+
+  state = {
+    users: [],
+    user: {
+      name: 'Name',
+      surname: 'Surname',
+      email: 'email@example.com'
+    }
+  }
+  
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    fetch('http://localhost:5000/')
+    .then(response => response.json())
+    .then(response => { this.setState({ users: response.data}) })
+    .catch(err => console.log(err))
+  }
+
+  removeUser = (id) => {
+    fetch('http://localhost:5000/remove?id=' + id)
+    .then(this.getUsers)
+    .catch(err => console.error(err))
+  }
+
+
+renderUser = ({_id, name, surname, email}) => <div key={_id}>{name} {surname} {email} <button onClick={() => this.removeUser(_id)}>x</button></div>
+
+  addUser = () => {
+    const {user} = this.state
+    console.log(user)
+    fetch(`http://localhost:5000/add?name=${user.name}&surname=${user.surname}&email=${user.email}`)
+      .then(this.getUsers)
+      .catch(err => console.error(err))
+  }
+
+
+
+  render() {
+    const { users, user } = this.state
+    return (
+      <>
+      <div> 
+        <label>Name:</label>
+        <input required value={user.name} onChange={e => this.setState({user: {...user, name:e.target.value}})}></input>
+        <label>Surname:</label>
+        <input value={user.surname} onChange={e => this.setState({user: {...user, surname:e.target.value}})} required></input>
+        <label>Email:</label>
+        <input type="email" value={user.email} onChange={e => this.setState({user: {...user, email:e.target.value}})} required></input>
+        <button onClick={this.addUser} type="submit">Submit</button>
+        <form onSubmit={this.addUser}>
+        </form>
+      </div>
+      
+
+
+      <div>
+        {users.map(this.renderUser)}
+      </div>
+      </>
+    )
+  }
 }
 
 export default App;
